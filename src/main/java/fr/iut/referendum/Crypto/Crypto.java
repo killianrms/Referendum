6a08d784446b9cd665dce0daa5de305fe0a4ba92
Maël NICOLAS <mael.nicolas@etu.umontpellier.fr>
2025-06-09 03:21:18 +0200

    feat: ZKProof in lib crypto

--- src/main/java/fr/iut/referendum/Crypto/Crypto.java
+++ src/main/java/fr/iut/referendum/Crypto/Crypto.java
@@ -1,19 +1,21 @@
 package fr.iut.referendum.Crypto;
 
 import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public abstract class Crypto {
     private static final SecureRandom random = new SecureRandom();
 
-    public static BigInteger[] encrypt(BigInteger m, BigInteger[] pk) {
+    public static BigInteger[][] encrypt(BigInteger m, BigInteger[] pk) {
         BigInteger p = pk[0];
         BigInteger g = pk[1];
         BigInteger h = pk[2];
         BigInteger k = new BigInteger(p.subtract(BigInteger.ONE).bitLength(), random).mod(p.subtract(BigInteger.ONE)); // k < p-1
         BigInteger c1 = g.modPow(k, p); // c1 = g^k mod p
         BigInteger c2 = g.modPow(m, p).multiply(h.modPow(k, p)).mod(p); // c2 = g^m * publickey^k mod p
-        return new BigInteger[]{c1, c2};
+        return new BigInteger[][] {{c1, c2}, {k}};
     }
 
     public static BigInteger[] genkey() {
@@ -60,6 +62,84 @@ public abstract class Crypto {
         }
         System.out.println("Déchiffrement échoué");
         return null;
+    }
+
+    // Zero Knowledge Protocol
+    public static BigInteger[] createZKProof(BigInteger m, BigInteger[] c, BigInteger[] pk, BigInteger r) throws NoSuchAlgorithmException {
+        BigInteger c1 = c[0];
+        BigInteger c2 = c[1];
+        BigInteger p = pk[0];
+        BigInteger g = pk[1];
+        BigInteger h = pk[2];
+
+        int mi = m.intValue();
+        int mj = 1 - m.intValue();
+
+        // False value
+        BigInteger[] A = new BigInteger[2];
+        BigInteger[] B = new BigInteger[2];
+        BigInteger[] chall = new BigInteger[2];
+        BigInteger[] rep = new BigInteger[2];
+
+        chall[mj] = new BigInteger(p.bitLength(), random).mod(p);
+        rep[mj] = new BigInteger(p.bitLength(), random).mod(p);
+
+        A[mj] = (g.modPow(rep[mj], p).mod(p)).multiply(c1.modPow(chall[mj], p)).mod(p);
+        B[mj] = ((h.modPow(rep[mj], p).mod(p)).multiply((c2.multiply(g.modInverse(p))).modPow(chall[mj], p)).mod(p)).mod(p);
+
+        // True value
+        BigInteger w = new BigInteger(p.bitLength(), random).mod(p);
+        A[mi] = g.modPow(w, p).mod(p);
+        B[mi] = h.modPow(w, p).mod(p);
+
+        BigInteger[] values = {c1, c2, A[0], B[0], A[1], B[1]};
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        for (BigInteger v : values) {
+            byte[] b = v.toByteArray();
+            md.update(b);
+        }
+
+        byte[] hashBytes = md.digest();
+        BigInteger hashInt = new BigInteger(1, hashBytes).mod(p); // e
+
+        chall[mi] = hashInt.subtract(chall[mj]).mod(p);
+        rep[mi] = w.subtract(chall[mj].multiply(r).mod(p)).mod(p);
+
+        return new BigInteger[]{chall[0], rep[0], chall[1], rep[1]};
+    }
+
+    public static boolean verifyZKProof(BigInteger[] c, BigInteger[] pk, BigInteger[] pi) throws NoSuchAlgorithmException {
+        BigInteger c1 = c[0];
+        BigInteger c2 = c[1];
+        BigInteger p = pk[0];
+        BigInteger g = pk[1];
+        BigInteger h = pk[2];
+
+        BigInteger chall0 = pi[0];
+        BigInteger rep0 = pi[1];
+        BigInteger chall1 = pi[2];
+        BigInteger rep1 = pi[3];
+
+        BigInteger A0 = (g.modPow(rep0, p).mod(p)).multiply(c1.modPow(chall0, p)).mod(p);
+        BigInteger A1 = (g.modPow(rep1, p).mod(p)).multiply(c1.modPow(chall1, p)).mod(p);
+        BigInteger B0 = (h.modPow(rep0, p).mod(p)).multiply(c2.modPow(chall0, p)).mod(p);
+        BigInteger B1 = (h.modPow(rep1, p).mod(p)).multiply((c2.divide(g).mod(p)).modPow(chall1, p)).mod(p);
+
+        BigInteger[] values = {c1, c2, A0, B0, A1, B1};
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        for (BigInteger v : values) {
+            byte[] b = v.toByteArray();
+            md.update(b);
+        }
+
+        BigInteger verif = chall0.add(chall1).mod(p);
+
+        byte[] hashBytes = md.digest();
+        BigInteger result = new BigInteger(1, hashBytes).mod(p);
+
+        return result.equals(verif);
     }
 
 }
